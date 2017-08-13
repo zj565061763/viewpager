6daf1bb6fb77b2e3006901cc5aa75262404e02db
zjun <565061763@qq.com>
2017-08-13 22:13:34 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/viewpager/MainActivity.java
+++ app/src/main/java/com/fanwe/www/viewpager/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.fanwe.library.SDLibrary;
 import com.fanwe.library.listener.SDSimpleIterateCallback;
 import com.fanwe.library.model.SelectableModel;
 import com.fanwe.library.utils.LogUtil;
 import com.fanwe.library.utils.SDCollectionUtil;
+import com.fanwe.library.utils.SDViewUtil;
 import com.fanwe.library.viewpager.SDViewPagerInfoListener;
 import com.fanwe.library.viewpager.SDViewPagerPlayer;
 import com.fanwe.library.viewpager.extend.SDSimpleIndicatorViewPager;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        SDLibrary.getInstance().init(getApplication());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mViewPager = (SDSimpleIndicatorViewPager) findViewById(R.id.vpg_content);
@@ -58,19 +61,18 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //设置指示器相关配置，以下为默认配置，可以覆盖库中的默认配置
+        mViewPager.getIndicatorConfig().imageResIdNormal = R.drawable.lib_vpg_ic_indicator_normal; //指示器正常状态图片
+        mViewPager.getIndicatorConfig().imageResIdSelected = R.drawable.lib_vpg_ic_indicator_selected; //指示器选中状态图片
+        mViewPager.getIndicatorConfig().widthSelected = SDViewUtil.dp2px(20); //指示器图片宽度
+        mViewPager.getIndicatorConfig().margin = getResources().getDimensionPixelSize(R.dimen.lib_vpg_indicator_margin); //指示器图片间隔
+
         //设置ViewPager参数
         mViewPager.getViewPager().setItemCountPerPage(9); //设置每页有9个数据
         mViewPager.getViewPager().setColumnCountPerPage(3); //设置每一页有3列
         mViewPager.getViewPager().setHorizontalDivider(getResources().getDrawable(R.drawable.divider_horizontal)); //设置横分割线
         mViewPager.getViewPager().setVerticalDivider(getResources().getDrawable(R.drawable.divider_vertical)); //设置竖分割线
         mViewPager.getViewPager().setGridAdapter(mItemAdapter); //设置适配器
-
-        //设置指示器相关配置，以下为默认配置，可以覆盖库中的默认配置
-        mViewPager.getIndicatorConfig().imageResIdNormal = R.drawable.lib_vpg_ic_indicator_normal; //指示器正常状态图片
-        mViewPager.getIndicatorConfig().imageResIdSelected = R.drawable.lib_vpg_ic_indicator_selected; //指示器选中状态图片
-        mViewPager.getIndicatorConfig().width = getResources().getDimensionPixelSize(R.dimen.lib_vpg_indicator_width); //指示器图片宽度
-        mViewPager.getIndicatorConfig().height = getResources().getDimensionPixelSize(R.dimen.lib_vpg_indicator_height); //指示器图片高度
-        mViewPager.getIndicatorConfig().margin = getResources().getDimensionPixelSize(R.dimen.lib_vpg_indicator_margin); //指示器图片间隔
 
     }
 
