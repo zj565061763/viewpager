b742fedf211f2772fa2081f7850928b42ec121ba
zjun <565061763@qq.com>
2017-08-24 09:29:03 +0800

    增加注释

--- lib/src/main/java/com/fanwe/library/viewpager/indicator/IPagerIndicatorGroupView.java
+++ lib/src/main/java/com/fanwe/library/viewpager/indicator/IPagerIndicatorGroupView.java
@@ -5,18 +5,59 @@ package com.fanwe.library.viewpager.indicator;
  */
 public interface IPagerIndicatorGroupView
 {
+    /**
+     * 设置适配器
+     *
+     * @param adapter
+     */
     void setAdapter(IPagerIndicatorAdapter adapter);
 
+    /**
+     * 返回适配器
+     *
+     * @return
+     */
     IPagerIndicatorAdapter getAdapter();
 
+    /**
+     * 返回position位置对应的ItemView
+     *
+     * @param position
+     * @return
+     */
     IPagerIndicatorItemView getItemView(int position);
 
+    /**
+     * 页数变化回调
+     *
+     * @param count
+     */
     void onPageCountChanged(int count);
 
+    /**
+     * ViewPager某一页进入回调
+     *
+     * @param position     第几页
+     * @param enterPercent 进入百分比
+     * @param leftToRight  true-ViewPager页从右边进入，false-ViewPager页从左边进入
+     */
     void onEnter(int position, float enterPercent, boolean leftToRight);
 
+    /**
+     * ViewPager某一页退出回调
+     *
+     * @param position     第几页
+     * @param leavePercent 退出百分比
+     * @param leftToRight  true-ViewPager页从左边退出，false-ViewPager页从右边退出
+     */
     void onLeave(int position, float leavePercent, boolean leftToRight);
 
+    /**
+     * ViewPager某一页选中或者非选中回调
+     *
+     * @param position 第几页
+     * @param selected true-选中，false-未选中
+     */
     void onSelectedChanged(int position, boolean selected);
 
 }
--- lib/src/main/java/com/fanwe/library/viewpager/indicator/IPagerIndicatorItemView.java
+++ lib/src/main/java/com/fanwe/library/viewpager/indicator/IPagerIndicatorItemView.java
@@ -7,11 +7,33 @@ import com.fanwe.library.viewpager.indicator.model.PositionData;
  */
 public interface IPagerIndicatorItemView
 {
+    /**
+     * 选中或者非选中回调
+     *
+     * @param selected true-选中，false-未选中
+     */
     void onSelectedChanged(boolean selected);
 
+    /**
+     * ViewPager页进入回调
+     *
+     * @param enterPercent 进入百分比
+     * @param leftToRight  true-ViewPager页从右边进入，false-ViewPager页从左边进入
+     */
     void onEnter(float enterPercent, boolean leftToRight);
 
+    /**
+     * ViewPager页退出回调
+     *
+     * @param leavePercent 退出百分比
+     * @param leftToRight  true-ViewPager页从左边退出，false-ViewPager页从右边退出
+     */
     void onLeave(float leavePercent, boolean leftToRight);
 
+    /**
+     * 返回ItemView的位置信息
+     *
+     * @return
+     */
     PositionData getPositionData();
 }
